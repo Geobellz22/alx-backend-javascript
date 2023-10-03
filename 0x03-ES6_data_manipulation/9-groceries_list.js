d2071ac5e76bf2348878179d38672f0a93bda341
Geobellz22 <st377126@gmail.com>
2023-10-03 23:40:29 +0000

    Task 9

---
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,10 @@
+export default function groceriesList() {
+  const values = [
+    ['Apples', 10],
+    ['Tomatoes', 10],
+    ['p`asta', 1],
+    ['Rice', 1],
+    ['Banana', 5],
+  ];
+  return new Map(values);
+}
