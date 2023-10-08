7ce79c1ac0d99702b1ae33d1a3633e4502348797
Geobellz22 <st377126@gmail.com>
2023-10-08 23:05:40 +0000

    Task 4

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,47 @@
+import Currency from './3-currency';
+
+export default class Pricing {
+  constructor(amount, currency) {
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(value) {
+    if (typeof value !== 'number') {
+      throw new TypeError('amount must be a number');
+    }
+    this._amount = value;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(value) {
+    if (!(value instanceof Currency)) {
+      throw new TypeError('currency must be a Currency');
+    }
+    this._currency = value;
+  }
+
+  displayFullPrice() {
+    const fullPrice = `${this.amount} ${this.currency.name} (${this.currency.code})`;
+    return fullPrice;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    if (typeof amount !== 'number') {
+      throw new TypeError('amount must be a number');
+    }
+
+    if (typeof conversionRate !== 'number') {
+      throw new TypeError('conversionRate must be a number');
+    }
+
+    return amount * conversionRate;
+  }
+}
