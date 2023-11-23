8374f2139808430bd1fb0d0b527fb3af6a7778be
Geobellz22 <st377126@gmail.com>
2023-11-23 14:48:15 +0000

    Task 2

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,45 +1,46 @@
 const fs = require('fs');
 
-const countStudents = (path) => {
-  const students = {};
-  const fields = {};
-  let fileLength = 0;
-
-  try {
-    const content = fs.readFileSync(path, 'utf-8');
-    const lines = content.split(/\r?\n/);
-
-    for (let i = 0; i < lines.length; i += 1) {
-      if (lines[i].trim() !== '') {
-        fileLength += 1;
-        const field = lines[i].split(',');
-
-        if (students[field[3]]) {
-          students[field[3]].push(field[0]);
-        } else {
-          students[field[3]] = [field[0]];
-        }
-
-        if (fields[field[3]]) {
-          fields[field[3]] += 1;
-        } else {
-          fields[field[3]] = 1;
-        }
-      }
-    }
-
-    const numberOfStudents = fileLength;
-    console.log(`Number of students: ${numberOfStudents}`);
-
-    for (const key of Object.keys(fields)) {
-      if (key !== 'field') {
-        console.log(`Number of students in ${key}: ${fields[key]}. List: ${students[key].join(', ')}`);
-      }
-    }
-  } catch (e) {
+/**
+ * Counts the students in a CSV data file.
+ * @param {String} dataPath The path to the CSV data file.
+ * @author Bezaleel Olakunori <https://github.com/B3zaleel>
+ */
+const countStudents = (dataPath) => {
+  if (!fs.existsSync(dataPath)) {
     throw new Error('Cannot load the database');
+  }
+  if (!fs.statSync(dataPath).isFile()) {
+    throw new Error('Cannot load the database');
+  }
+  const fileLines = fs
+    .readFileSync(dataPath, 'utf-8')
+    .toString('utf-8')
+    .trim()
+    .split('\n');
+  const studentGroups = {};
+  const dbFieldNames = fileLines[0].split(',');
+  const studentPropNames = dbFieldNames.slice(0, dbFieldNames.length - 1);
+
+  for (const line of fileLines.slice(1)) {
+    const studentRecord = line.split(',');
+    const studentPropValues = studentRecord.slice(0, studentRecord.length - 1);
+    const field = studentRecord[studentRecord.length - 1];
+    if (!Object.keys(studentGroups).includes(field)) {
+      studentGroups[field] = [];
+    }
+    const studentEntries = studentPropNames
+      .map((propName, idx) => [propName, studentPropValues[idx]]);
+    studentGroups[field].push(Object.fromEntries(studentEntries));
+  }
+
+  const totalStudents = Object
+    .values(studentGroups)
+    .reduce((pre, cur) => (pre || []).length + cur.length);
+  console.log(`Number of students: ${totalStudents}`);
+  for (const [field, group] of Object.entries(studentGroups)) {
+    const studentNames = group.map((student) => student.firstname).join(', ');
+    console.log(`Number of students in ${field}: ${group.length}. List: ${studentNames}`);
   }
 };
 
 module.exports = countStudents;
-
