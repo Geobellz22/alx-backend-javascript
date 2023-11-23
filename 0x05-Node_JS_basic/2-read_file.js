034ebde4b311f41681a5f6182d9cccfea32edeec
Geobellz22 <st377126@gmail.com>
2023-11-23 12:15:06 +0000

    task 2

---
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -0,0 +1,44 @@
+const fs = require('fs');
+
+const countStudents = (path) => {
+  const students = {};
+  const fields = {};
+  let fileLength = 0;
+
+  try {
+    const content = fs.readFileSync(path, 'utf-8');
+    const lines = content.split('\r\n');
+
+    for (let i = 0; i < lines.length; i += 1) {
+      if (lines[i].trim() !== '') {
+        fileLength += 1;
+        const field = lines[i].split(',');
+
+        if (students[field[3]]) {
+          students[field[3]].push(field[0]);
+        } else {
+          students[field[3]] = [field[0]];
+        }
+
+        if (fields[field[3]]) {
+          fields[field[3]] += 1;
+        } else {
+          fields[field[3]] = 1;
+        }
+      }
+    }
+
+    const numberOfStudents = fileLength - 1;
+    console.log(`Number of students: ${numberOfStudents}`);
+
+    for (const key of Object.keys(fields)) {
+      if (key !== 'field') {
+        console.log(`Number of students in ${key}: ${fields[key]}. List: ${students[key].join(', ')}`);
+      }
+    }
+  } catch (e) {
+    throw new Error('Cannot load the database');
+  }
+};
+
+module.exports = countStudents;
